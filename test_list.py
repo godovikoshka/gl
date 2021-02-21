2cb21c71f11ff4f7e82c1dfec733f0714b8ad713
godovikoshka <godovikoshka@gmail.com>
2021-02-21 21:07:48 +0200

    change_mistakes

--- test_list.py
+++ test_list.py
@@ -28,6 +28,11 @@ def delete(list):
     return list
 
 
+def del_third_element(list):
+    list.remove(list[-4])
+    return list
+
+
 class TestListMethods(unittest.TestCase):
     def test_liubov_peleshenko_fi_94(self):
         list = [1, 2, 3, 4, 5, 6]
